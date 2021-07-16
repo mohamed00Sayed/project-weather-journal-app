4706f42810ce8301c7ec1ed15d3cc8efca685cbd
mohamed00Sayed <87094481+mohamed00Sayed@users.noreply.github.com>
2021-07-16 15:53:07 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+const app = express();
+/* Middleware*/
+const bodyParser = require('body-parser');
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+// Initialize the main project folder
+app.use(express.static('website'));
+app.use(cors());
+
+// Setup Server
+const port = 5556;
+const server = app.listen(port, serverCallBack);
+// the callBack function for the server
+function serverCallBack(){
+	console.log(`the server is running on port ${port}`);
+}
+
+app.get('/data', returnData);
+
+function returnData(req, res){
+	if(projectData !== null){
+		res.send(projectData);
+	}else{
+		res.send('No data available yet !');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
